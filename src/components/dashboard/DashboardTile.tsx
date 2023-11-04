17aab332cd331ba6480aab109d5788cddf5a7c21
“Arun <arunjacob2@gmail.com>
2023-11-04 03:42:48 -0400

    dashboard tiles to show count and cost basic

---
+++ src/components/dashboard/DashboardTile.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import useFetchData from "../../hooks/generic/useFetch";
+
+type DashboardDataType = {
+  diapers: { count: number; cost: number };
+  formula: { count: number; cost: number };
+};
+
+type DashBoardData = {
+  data: DashboardDataType;
+};
+
+type DataKey = keyof DashboardDataType;
+
+const DashboardTile = () => {
+  const { data, loading, error } = useFetchData<DashBoardData>("/dashboard");
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <div className="p-4 md:w-1/2 lg:w-1/3 xl:w-1/4 w-full">
+      <div className="bg-white rounded-lg shadow-md p-6">
+        {data?.data &&
+          Object.keys(data.data).map((itemKey, index) => {
+            const key = itemKey as DataKey;
+            const itemData = data.data[key];
+
+            if (itemData && typeof itemData === "object") {
+              return (
+                <div key={index}>
+                  <h2 className="text-xl font-semibold mb-4">{key}</h2>
+                  <div className="flex justify-between mb-2">
+                    <span className="text-primary">Count:</span>
+                    <span>{itemData.count}</span>
+                  </div>
+                  <div className="flex justify-between mb-2">
+                    <span className="text-secondary">Cost:</span>
+                    <span>${itemData.cost}</span>
+                  </div>
+                </div>
+              );
+            }
+
+            return null; // Skip rendering if itemData is not an object or is null
+          })}
+      </div>
+    </div>
+  );
+};
+
+export default DashboardTile;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import PageLayout from "../components/router-layouts/PageLayout";
+import { FaUserAlt, FaClinicMedical, FaLayerGroup } from "react-icons/fa";
+import PanelButton from "../components/PanelButton";
+import Panel from "../components/Panel";
+import DashboardTile from "../components/dashboard/DashboardTile";
+function Home() {
+  return (
+    <PageLayout>
+      <>
+        <Panel>
+          <PanelButton to="/users" text="Users">
+            <FaUserAlt className="text-secondary" />
+          </PanelButton>
+          <PanelButton to="/tests" text="Tests">
+            <FaClinicMedical className="text-secondary"></FaClinicMedical>
+          </PanelButton>
+          <PanelButton to="/testgroups" text="Groups">
+            <FaLayerGroup className="text-secondary"></FaLayerGroup>
+          </PanelButton>
+        </Panel>
+        <div className="container mx-auto p-4">
+          <div className="flex flex-wrap -mx-4">
+            <DashboardTile />
+          </div>
+        </div>
+      </>
+    </PageLayout>
+  );
+}
+
+export default Home;
